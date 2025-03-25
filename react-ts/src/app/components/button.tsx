6128ae12125cfeac8d90b4ccf2f9d3f3b295bc7c
Oskar Majcen <leagueofdisynk@gmail.com>
2025-03-25 16:09:10 +0100

    Added NextJS React project

---
+++ react-ts/src/app/components/button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+    return (
+        <button className="bg-blue-500 text-white rounded px-4 py-2">
+            Click me
+        </button>
+    )
+}
